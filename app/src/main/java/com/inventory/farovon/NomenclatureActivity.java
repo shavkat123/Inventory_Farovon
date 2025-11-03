b3942c89db6cec58510e04b164cfa10349f87d14
shavkat_0666 <shavkat_0666@mail.ru>
2025-11-03 13:30:09 +0500

    feat(issue-from-warehouse): Add scanner power dialog and improve UI
    This commit introduces several improvements to the "Issuing from warehouse" screen based on user feedback:
    
    1.  **Scanner Power Dialog:** A dialog has been added to allow the user to select the scanner power from 1 to 10. This is triggered by a new scanner icon button in the "ОУ" (Assets) section.
    
    2.  **UI Improvements:**
        *   The main "Create" button is now hidden when the "ОУ" tab is selected, preventing it from overlapping with the local buttons in that fragment.
        *   The buttons in the "ОУ" section ("Scanner" and "Select") are now properly displayed.
    
    This was implemented by:
    - Creating a new layout file `dialog_scanner_power.xml` for the dialog.
    - Adding logic to `AssetsFragment` to show the dialog and handle the power selection.
    - Adding an `OnTabSelectedListener` to the `TabLayout` in `IssueFromWarehouseActivity` to control the visibility of the "Create" button.

--- app/src/main/java/com/inventory/farovon/NomenclatureActivity.java
+++ app/src/main/java/com/inventory/farovon/NomenclatureActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import com.google.android.material.button.MaterialButton;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
